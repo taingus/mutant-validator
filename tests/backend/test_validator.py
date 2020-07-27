76ee14d791f156c44be9011d0fb52e93e4b60676
Agustín Cignetti <cignettiagustin@gmail.com>
2020-07-27 19:51:17 -0300

    Base tests for horizontal search

--- tests/backend/test_validator.py
+++ tests/backend/test_validator.py
@@ -1,5 +1,25 @@
-def test_valid_DNA_sequence_horizontal():
-    pass
+from mutant_validator.backend.validator import is_mutant
+from mutant_validator.backend.containers import DNASequence
+
+
+def test_valid_DNA_sequence_horizontal_first_occurence():
+    assert is_mutant(DNASequence("AAAATGA")) is True
+
+
+def test_valid_DNA_sequence_horizontal_second_occurence():
+    assert is_mutant(DNASequence("TAAAAGA")) is True
+
+
+def test_valid_DNA_sequence_horizontal_third_occurence():
+    assert is_mutant(DNASequence("TGAAAAA")) is True
+
+
+def test_valid_DNA_sequence_horizontal_fourth_occurence():
+    assert is_mutant(DNASequence("TGTAAAA")) is True
+
+
+def test_valid_DNA_sequence_horizontal_not_mutant():
+    assert is_mutant(DNASequence("TGTAAAT")) is False
 
 
 def test_valid_DNA_sequence_vertical():
