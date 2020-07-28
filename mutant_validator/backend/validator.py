2e268be6eaaa17118f124035271667c730cc73dd
Agustín Cignetti <cignettiagustin@gmail.com>
2020-07-28 18:23:45 -0300

    Forward search working

--- mutant_validator/backend/validator.py
+++ mutant_validator/backend/validator.py
@@ -13,16 +13,33 @@ def is_mutant(sequence: Union[List[str], DNA]):
     if isinstance(sequence, list):
         sequence = DNA(dna=sequence)
 
-    for line in sequence.dna:
-        node = Node(h=line)
+    v_len = len(sequence.dna)
+    h_len = len(sequence.dna[0])
+
+    for pos, line in enumerate(sequence.dna):
+        f_line = "".join(
+            [
+                sequence.dna[x][pos + x]
+                for x in range(0, v_len - pos)
+                if pos + x <= h_len <= v_len
+            ]
+        )
+        node = Node(h=line, f=f_line)
 
         if node.is_mutant():
             return True
 
-    v_len = len(sequence.dna)
     for col in range(0, len(sequence.dna[0])):
-        line = "".join([sequence.dna[x][col] for x in range(0, v_len)])
-        node = Node(v=line)
+        v_line = "".join([sequence.dna[x][col] for x in range(0, v_len)])
+        f_line = "".join(
+            [
+                sequence.dna[col + x][x]
+                for x in range(0, h_len - col)
+                if col + x <= h_len <= v_len
+            ]
+        )
+        node = Node(v=v_line, f=f_line)
+
         if node.is_mutant():
             return True
 
