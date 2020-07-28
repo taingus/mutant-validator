980bfa0e0d7ba2e9cab2b14d79c4367f403474dd
Agustín Cignetti <cignettiagustin@gmail.com>
2020-07-27 21:43:09 -0300

    Allow DNA object as parameter

--- mutant_validator/backend/validator.py
+++ mutant_validator/backend/validator.py
@@ -1,14 +1,24 @@
-from typing import List
+from typing import (
+    List,
+    Union,
+)
 
-from .containers import DNA, Node
+from .containers import (
+    DNA,
+    Node,
+)
 
 
-def is_mutant(sequence: List[str]):
-    dna = DNA(dna=sequence)
+def is_mutant(sequence: Union[List[str], DNA]):
+    if isinstance(sequence, list):
+        sequence = DNA(dna=sequence)
 
-    for line in dna.dna:
+    for line in sequence.dna:
         node = Node(h=line)
 
         if node.is_mutant():
             return True
+
+    for x in range(0, len(sequence.dna[0])):
+        pass
     return False
