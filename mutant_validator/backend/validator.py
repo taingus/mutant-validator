4045f5c8b483fb53c5d4c3877d3a2716b1529256
Agustín Cignetti <cignettiagustin@gmail.com>
2020-07-27 21:54:14 -0300

    Add vertical validation. Horrible, very horrible

--- mutant_validator/backend/validator.py
+++ mutant_validator/backend/validator.py
@@ -19,6 +19,11 @@ def is_mutant(sequence: Union[List[str], DNA]):
         if node.is_mutant():
             return True
 
-    for x in range(0, len(sequence.dna[0])):
-        pass
+    v_len = len(sequence.dna)
+    for col in range(0, len(sequence.dna[0])):
+        line = "".join([sequence.dna[x][col] for x in range(0, v_len)])
+        node = Node(v=line)
+        if node.is_mutant():
+            return True
+
     return False
