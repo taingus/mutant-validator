771d168a732673030b2cec15c0a0656de7f5d4af
Agustín Cignetti <cignettiagustin@gmail.com>
2020-07-27 21:24:40 -0300

    Add horizontal sequence tests

--- mutant_validator/backend/validator.py
+++ mutant_validator/backend/validator.py
@@ -6,11 +6,8 @@ from .containers import DNA, Node
 def is_mutant(sequence: List[str]):
     dna = DNA(dna=sequence)
 
-    h_len = len(dna.dna[0])
-    v_len = len(dna.dna)
-
     for line in dna.dna:
-        node = Node(h=line, v_len=v_len, h_len=h_len)
+        node = Node(h=line)
 
         if node.is_mutant():
             return True
--- tests/backend/test_validator.py
+++ tests/backend/test_validator.py
@@ -1,4 +1,3 @@
-from mutant_validator.backend.containers import DNASequence
 from mutant_validator.backend.validator import is_mutant
 
 
@@ -22,8 +21,32 @@ def test_valid_DNA_sequence_horizontal_not_mutant():
     assert is_mutant(["TGTAAAT"]) is False
 
 
-def test_valid_DNA_sequence_vertical():
-    pass
+def test_valid_DNA_sequence_vertical_first_column(generate_vertical_dna):
+    assert is_mutant(generate_vertical_dna(column=1)) is True
+
+
+def test_valid_DNA_sequence_vertical_second_column(generate_vertical_dna):
+    assert is_mutant(generate_vertical_dna(column=2)) is True
+
+
+def test_valid_DNA_sequence_vertical_third_column(generate_vertical_dna):
+    assert is_mutant(generate_vertical_dna(column=3)) is True
+
+
+def test_valid_DNA_sequence_vertical_first_column_second_line(generate_vertical_dna):
+    assert is_mutant(generate_vertical_dna(column=1, line=2)) is True
+
+
+def test_valid_DNA_sequence_vertical_second_column_third_line(generate_vertical_dna):
+    assert is_mutant(generate_vertical_dna(column=2, line=3)) is True
+
+
+def test_valid_DNA_sequence_vertical_third_column_fourth_line(generate_vertical_dna):
+    assert is_mutant(generate_vertical_dna(column=3, line=4)) is True
+
+
+def test_valid_DNA_sequence_no_mutant(generate_vertical_dna):
+    assert is_mutant(generate_vertical_dna(column=0, line=0)) is False
 
 
 def test_valid_DNA_sequence_diagonal_forward():
