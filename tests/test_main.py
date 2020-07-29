8f3de91b92b67483b5300038b617760e57cdb084
Agustín Cignetti <cignettiagustin@gmail.com>
2020-07-29 19:46:54 -0300

    Configure testing DB

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,28 @@
+from databases import Database
 from fastapi import status
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
 
+from mutant_validator.backend.database import (
+    get_db,
+    metadata,
+)
 from mutant_validator.main import app
+
+TEST_DATABASE_URL = "sqlite:///mutant_validator/test_db.sqlite3"
+
+db_test = Database(TEST_DATABASE_URL, force_rollback=True)
+engine = create_engine(TEST_DATABASE_URL, connect_args={"check_same_thread": False})
+
+metadata.create_all(engine)
+
+
+def override_get_db() -> Database:
+    return db_test
+
+
+app.dependency_overrides[get_db] = override_get_db
+
 
 client = TestClient(app)
 
