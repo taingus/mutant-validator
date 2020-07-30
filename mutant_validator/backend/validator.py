7387d437e7358ef9ad3acf704eb281fc046a587c
Agustín Cignetti <cignettiagustin@gmail.com>
2020-07-30 18:59:36 -0300

    Use better boundaries checks

--- mutant_validator/backend/validator.py
+++ mutant_validator/backend/validator.py
@@ -20,19 +20,19 @@ def is_mutant(sequence: Union[List[str], DNA]):
         for pos, line in enumerate(sequence.dna):
             f_line = ""
             b_line = ""
-            if v_len >= 4 and h_len >= 4:
+            if v_len > 3:
                 f_line = "".join(
                     [
                         sequence.dna[pos + x][x]
                         for x in range(0, v_len - pos)
-                        if pos + x <= h_len <= v_len
+                        if pos + x < h_len and pos + x < v_len
                     ]
                 )
                 b_line = "".join(
                     [
                         sequence.dna[pos + x][v_len - 1 - x]
                         for x in range(0, v_len - pos)
-                        if h_len - x >= 0 and v_len - x >= 0
+                        if h_len > x and v_len > x
                     ]
                 )
             node = Node(h=line, f=f_line, b=b_line)
@@ -44,12 +44,12 @@ def is_mutant(sequence: Union[List[str], DNA]):
         for col in range(0, len(sequence.dna[0])):
             f_line = ""
             b_line = ""
-            if v_len >= 4 and h_len >= 4:
+            if h_len > 3:
                 f_line = "".join(
                     [
                         sequence.dna[x][col + x]
                         for x in range(0, h_len - col)
-                        if col + x <= h_len <= v_len
+                        if col + x < h_len and col + x < v_len
                     ]
                 )
                 b_line = "".join(
