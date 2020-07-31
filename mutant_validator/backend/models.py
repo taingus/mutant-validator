01c053588161ad334625dca4c22aae37aefad747
Agustín Cignetti <cignettiagustin@gmail.com>
2020-07-30 23:02:42 -0300

    Adjust for mysql

--- mutant_validator/backend/models.py
+++ mutant_validator/backend/models.py
@@ -5,7 +5,7 @@ from mutant_validator.backend.database import metadata
 validated_dna = sqlalchemy.Table(
     "validated_dnas",
     metadata,
-    sqlalchemy.Column("sha", sqlalchemy.String, primary_key=True),
-    sqlalchemy.Column("dna", sqlalchemy.String),
+    sqlalchemy.Column("sha", sqlalchemy.String(length=65), primary_key=True),
+    sqlalchemy.Column("dna", sqlalchemy.Text),
     sqlalchemy.Column("mutant", sqlalchemy.Boolean),
 )
